86a457c995015e8acbd19183bad3ed334f7a18a2
JaegerJochimsen <75139372+JaegerJochimsen@users.noreply.github.com>
2020-11-30 11:01:04 -0800

    Update bivariate.py

--- bivariate.py
+++ bivariate.py
@@ -43,7 +43,7 @@ def Bivariate(v_matrix: np.matrix, x: np.double, y: np.double):
     # [[1, 2],        # 1 is element (0,0), 2 is element (0,1), 4 is element (1,1), etc.
     # [3, 4]]
     Ex/
-    >>> v_matrix = [[1,2], [3,4]]
+    >>> v_matrix = np.matrix([[1,2], [3,4]])
 
     >>> Bivariate(v_matrix, 1, 1)
     10
