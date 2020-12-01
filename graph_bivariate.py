11b0d7184350ff6ce4ebb0eb2873c06e5d01702c
JaegerJochimsen <75139372+JaegerJochimsen@users.noreply.github.com>
2020-11-30 17:46:54 -0800

    Update graph_bivariate.py

--- graph_bivariate.py
+++ graph_bivariate.py
@@ -1,41 +1,38 @@
-"""
-Project: PolySolvy Version X
-Math 351 - Numerical Analysis
-Under the instruction of Professor Eric Merchant
-
-Team: Nathan, Jaeger, Ronnie
-
-File Description:
-graph_bivariate.py uses the functionality of bivariate.py to generate a 3D surface plot of an interpolating function
-and the points it interpolates (of the (x,y,z)). Graph_Bivariate() generates a graph which displays the surface and
-runCounter.py
-"""
-import bivariate 
-import numpy as np
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-
 def Graph_Bivariate(vmatrix: np.matrix, x: np.array, y:np.array, original_z: np.array):
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    z = []
+    
+    calculated_z = []
+    
+    # generate some sample x,y vals to plot, this helps to fill out the curve
+    sample_x = np.arange(x.min() - 1,x.max() + 1,.1)
+    sample_y = np.arange(y.min() - 1, y.max() + 1, .1)
 
-    # Create the array of z values based off of constants in vmatrix and x and y vals
-    for index in range(len(x)):
-        z.append(Bivariate(vmatrix, np.double(x[index]),np.double(y[index])))
-    z = np.array(z)
+    # make sure to include our original x's and y's as well
+    for i in range(len(x)):
+        sample_x = np.append(sample_x, x[i])
+        sample_y = np.append(sample_y, y[i])
 
-    #graph surface via trisurf in blue and overlay points in red
-    ax.plot_trisurf(x,y,z,linewidth=0, triangles='triangles')
-    ax.scatter(x,y,z, c='red')
+    # Create the array of z values based off of constants in vmatrix and original x and y vals
+    for index in range(len(sample_x)):
+        calculated_z.append(np.double(Bivariate(vmatrix, sample_x[index],sample_y[index])))
+        
+    # convert to correct data type
+    calculated_z = np.array(calculated_z)
 
+    # a plot of our parametric curve overlayed over our original data points
+    ax.plot(sample_x,sample_y,calculated_z)
+    ax.scatter(x,y,original_z, c='red')
 
 # ------------------- #
 #   A Neat Example    #
 # ------------------- #
-v_matrix = np.matrix([[8/3,-7/3],[0,2/3]])    
-x = np.array([1,3,4,4]) 
-y = np.array([1,2,1,4])   
-zs = np.array([1,2,3,4]) 
-Graph_Bivariate(v_matrix, x, y, zs)
 
+# coeffs calculated using gaussian elim
+v_matrix = np.matrix([[8/3,-7/3],[0,2/3]])   
+
+# our original x's, y's, and z's
+xs = np.array([1,3,4,4]) 
+ys = np.array([1,2,1,4])
+zs = np.array([1,2,3,4]) 
+Graph_Bivariate(v_matrix, xs, ys, zs)
