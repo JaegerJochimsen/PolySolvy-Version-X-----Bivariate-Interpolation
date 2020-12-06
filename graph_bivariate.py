06278700464acc93f8ab03ed0fb62678e497ab5d
JaegerJochimsen <75139372+JaegerJochimsen@users.noreply.github.com>
2020-12-06 10:35:59 -0800

    Updated docstring and code changes
    - Updated vmatrix -> vvector
    - added fixup for odd-sized data sets

--- graph_bivariate.py
+++ graph_bivariate.py
@@ -3,54 +3,52 @@ import bivariate
 import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
+import random
 %matplotlib notebook
 
-def Graph_Bivariate(vmatrix: np.matrix, orig_x: np.array, orig_y:np.array, orig_z: np.array):
+def Graph_Bivariate(vvector: np.array, orig_x: np.array, orig_y:np.array, orig_z: np.array):
     """
     A function which plots an original set of (x,y,z) points in a 3D space and overlays that graph with a graph
     of the function output by Bivariate() (this is the interpolating function for the original (x,y,z) points).
-
-    param:  vmatrix: an np.matrix of coefficients of the terms of the interpolating polynomial. These coefficients
+   
+   param:  vvector: an np.array of coefficients of the terms of the interpolating polynomial. These coefficients
             are of the form:
-            [[c,y,y^2,y^3,...........................y^(n//4)],         // powers of y increase horizontally, powers of x vertically.
-             [x, xy, xy^2,.........................,xy^(n//4)],
-             [x^2,(x^2)y,(x^2)y^2,...........................]
-             [x^3,(x^3)y,....................................]
-             ................................................]
-             ................................................]
-             [(x^(n//4)),(x^(n//4))y,..., (x^(n//4))(y^(n//4)]]
-
+            [c,y,y^2,y^3,...........................y^(n//4),         // powers of y increase horizontally, powers of x vertically.
+             x, xy, xy^2,.........................,xy^(n//4),
+             x^2,(x^2)y,(x^2)y^2,...........................
+             x^3,(x^3)y,....................................
+             ................................................
+             ................................................
+             (x^(n//4)),(x^(n//4))y,..., (x^(n//4))(y^(n//4)]
+             
              These coefficients are used by Bivariate() to compute the corresponding z-value for a given (x,y) pair as
              specified by the interpolating polynomial.
-
+             
     param: x: an np.array of the original x-coordinates that were interpolated.
     param: y: an np.array of the original y-coordinates that were interpolated.
     param: z: an np.array of the original z-cooordinates that were interpolated.
-
+    
     return: outputs a graph plotted using Axes3D from mpl_toolkits.mplot3d
-
+    
     libraries/files used:
     - bivariate.py
     - numpy
     - matplotlib.pyplot
     - mpl_toolkits.mplot3d
+    - random
     """
     # basic error catching
     # input arrays must be of equal size so that they represent viable points
     if (len(orig_x) != len(orig_y)) or (len(orig_x) != len(orig_z)):
         raise Exception("Input arrays are of unequal length")
     
-    # matrix of coeffs should be of size sqrt(n) where n is the number of points/coordinates being used
-    if len(vmatrix) != math.sqrt(len(orig_x)):
-        raise Exception("Coefficient matrix (vmatrix) is of incorrect size")
-    
     # set up the initial graphing space and initialize as a 3d space
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    
+   
     calculated_z = []
     
-   # generate some sample x,y vals to plot, this helps to fill out the curve
+    # generate some sample x,y vals to plot, this helps to fill out the curve
     # make sure that the range of value for the x and y's are the same length
     # take the larger of the two ranges for this purpose
     if (orig_x.max() - orig_x.min()) > (orig_y.max() - orig_y.min()):
@@ -60,27 +58,39 @@ def Graph_Bivariate(vmatrix: np.matrix, orig_x: np.array, orig_y:np.array, orig_
     else:
         sample_x = np.arange(orig_y.min(),orig_y.max()+1,.1)
         sample_y = np.arange(orig_y.min(), orig_y.max() + 1, .1)
-
+        
     # ensure that we include the original points
     sample_x = np.append(sample_x, orig_x)
     sample_y = np.append(sample_y, orig_y)
-
+    
     # Create the array of z values based off of constants in vmatrix and original x and y vals
     for index in range(len(sample_x)):
-        calculated_z.append(np.double(bivariate.Bivariate(vmatrix, sample_x[index],sample_y[index])))
-
+        calculated_z.append(np.double(Bivariate(vvector, sample_x[index],sample_y[index])))
+        
     # convert to correct data type
-    calculated_z = np.array(calculated_z)
-
+    calculated_z = np.array(calculated_z, dtype=np.double)
+    
+    # if an odd num of data points, add one to make even
+    if len(sample_x) % 2 != 0:
+        newX = random.randint(orig_x.min(), orig_x.max())
+        sample_x = np.append(sample_x, newX)
+        
+        newY = random.randint(orig_y.min(), orig_y.max())
+        sample_y = np.append(sample_y, newY)
+        
+        newZ = Bivariate(vmatrix, newX, newY)
+        calculated_z = np.append(calculated_z, newZ)
+    
     # reshape the data for the purpose of surface plotting
     reshapedx = sample_x.reshape(2, len(sample_x)//2)
     reshapedy = sample_y.reshape(2, len(sample_y)//2)
-    reshapedz = calculated_z.reshape(2, len(calculated_z)//2)
+    length = len(calculated_z) // 2
+    reshapedz = calculated_z.reshape(2, length)
     
     # surface plot
-    ax.plot_surface(reshapedx, reshapedy, reshapedz)
-    
-    # a plot of original data points
+    ax.plot_trisurf(sample_x, sample_y, calculated_z)
+
+    # plot of original data points
     ax.scatter(orig_x, orig_y, orig_z, c='red')
     
     # rotate the axes and update for an interactive graph
@@ -88,25 +98,3 @@ def Graph_Bivariate(vmatrix: np.matrix, orig_x: np.array, orig_y:np.array, orig_
         ax.view_init(30, angle)
         plt.draw()
         plt.pause(.001)
-
-# ------------------------ #
-#   A Few Neat Examples    #
-# ------------------------ #
-
-# coeffs calculated using gaussian elim
-v_matrix = np.matrix([[(9/8), (-11/16)],[(3/8),(3/16)]])   # here our coeffs are in the order: [[c, y], [x,xy]] (see docstring for example)
-
-# our original x's, y's, and z's
-xs = np.array([1,3,4,5]) 
-ys = np.array([1,2,6,4])
-zs = np.array([1,2,3,4]) 
-Graph_Bivariate(v_matrix, xs, ys, zs)
-
-
-# Here is a good example, uses evenly spaced points without repeats
-xs2 = np.array([1,2,3,4])
-ys2 = np.array([5,6,7,8])
-zs2 = np.array([9,10,11,12])
-v_matrix = np.matrix([[-4,3],[-2,0]])
-Graph_Bivariate(v_matrix,xs2,ys2,zs2)
-
